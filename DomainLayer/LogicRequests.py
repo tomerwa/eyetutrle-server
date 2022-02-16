2ce628f5273b0c740ab3fe035045f6e0cb470f4f
Yoel Katzir <yoelkatzir@gmail.com>
2022-02-16 09:48:02 -0800

    Read and write JSON

--- DomainLayer/LogicRequests.py
+++ DomainLayer/LogicRequests.py
@@ -1,7 +1,44 @@
+import json
+from Question import Questions
+
+QUESTIONS = '..\\questions.json'
+
 class LogicRequests:
 
     def __init__(self):
         self.user = None
+        self.questions = LogicRequests.read_questions()
+
+    @staticmethod
+    def read_from_json(path: str):
+        data = dict()
+        with open(path, encoding='utf8') as f:
+            data = json.load(f)
+        return data
+    
+    @staticmethod
+    def write_to_json(path: str, data: dict):
+        with open(path, 'w', encoding='utf8') as f:
+            json.dump(data, f)
+
+    @staticmethod
+    def read_questions():
+        questions = list()
+        data = LogicRequests.read_from_json(QUESTIONS)
+        for key,val in data.items():
+            questions.append(Questions(key, str(val['text']), list(val['answers']), int(val['correct'])))
+        return questions
+    
+    @staticmethod
+    def write_questions(questions: list):
+        data = dict()
+        for q in questions:
+            data[q.id] = {
+                'text': q.content,
+                'answers': q.possible_answers,
+                'correct': q.correct_answer
+            }
+        LogicRequests.write_to_json(QUESTIONS, data)
 
     @staticmethod
     def valid_id(id):

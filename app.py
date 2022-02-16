49b99842ad3e060fd10ab4968acc2ce6f2b0de19
Yoel Katzir <yoelkatzir@gmail.com>
2022-02-16 12:26:23 -0800

    TODO

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/login")
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     return 'Hello World!'
 
+# login -> 0, 1, 2
+# get_questions -> {a: question, b: question, c: question}
+# get_all_questions
+# tts (full question)
+# end_test -> grade
+# get_grades
+# add_question
+# remove_question
 
 if __name__ == '__main__':
     app.run()
